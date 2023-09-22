6f6d8829186b98507d66b0287d4b83dcb495a0f7
Henry <121696146+RedKZX@users.noreply.github.com>
2023-09-22 17:43:52 +0100

    Create indexcode.js

---
+++ indexcode.js
@@ -0,0 +1,17 @@
+//top of index where u define 
+
+const { DisTube } = require("distube");
+const { SpotifyPlugin } = require('@distube/spotify');
+const { SoundCloudPlugin } = require('@distube/soundcloud');
+const { YtDlpPlugin } = require('@distube/yt-dlp');
+
+//anywehre in index after file reading and whatever else
+
+//Distube Client
+
+client.distube = new DisTube(client, {
+    emitNewSongOnly: false,
+    leaveOnFinish: false,
+    emitAddSongWhenCreatingQueue: false,
+    plugins: [new SpotifyPlugin()]
+  });
